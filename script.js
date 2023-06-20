2eec60a65ae9eb23c6c027d94ab20262def6a5be
vishalbhoyar24 <124866977+vishalbhoyar24@users.noreply.github.com>
2023-06-20 17:28:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function volume_sphere() {
     //Write your code here
+	 var volume;
+    var radius = document.getElementById('radius').value;
+    radius = Math.abs(radius);
+    volume = (4/3) * Math.PI * Math.pow(radius, 3);
+    volume = volume.toFixed(4);
+    document.getElementById('volume').value = volume;
+    return false;
   
 } 
 
